ac04afe4d30f41951a59cd77f7572416574e5d2c
xiongmeiqiang123 <xiongmeiqiang@kwaishou.com>
2023-12-18 21:27:27 +0800

    添加readme

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { Iframe } from "./iframe-builder";
 // import 'subjx/dist/style/subjx.css';
 
 async function main() {
-    // const data = await window.prompt('请输入地址');
-    const data = 'http://localhost:5173/main/task-center';
+    const data = await window.prompt('请输入地址');
+    // const data = 'http://localhost:5173/main/task-center';
     if (data) {
         const iframe = new Iframe();
         (window as any).currentIframe = iframe;
